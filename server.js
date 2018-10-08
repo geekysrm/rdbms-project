5bb204dd8a9533c626a8b17e523ce09d52bded24
sonu <sonusaikishan@gmail.com>
2018-10-08 19:19:24 +0530

    Added NodeJS server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 8000;
+
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, 'build')));
+
+app.get("/api/hello",(req,res)=>{
+    res.send("hello");
+});
+
+// pass all other routes to React that will be handled by React Router
+app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}!`);
+});
+  
