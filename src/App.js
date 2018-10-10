718b1afbd2bdc98fa806a91a16fdcf3ce2d11948
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2018-10-10 10:08:53 +0530

    Changed app name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Farmer Assistance App</h1>
+        <h1>Car Insurance Assistance App</h1>
       </div>
     );
   }
