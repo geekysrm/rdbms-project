11680d92a9bde24d9fef62af3b6423b4df067132
Mandeep Sahoo <princemandeep.sahoo29@gmail.com>
2018-10-29 17:27:52 +0530

    Update car.py

--- ML/car.py
+++ ML/car.py
@@ -2,16 +2,22 @@ from keras.models import load_model
 import urllib.request
 import os
 import numpy as np
+import validators
+
+
 
 cwd = os.getcwd()
 
 model = load_model('final1.h5')
-
-url = 'https://upload.wikimedia.org/wikipedia/commons/4/46/Car_Accident.jpg'
-name = os.path.basename(url)
-full_name = cwd + name
-urllib.request.urlretrieve(url,full_name)
-path = '/home/mandeep/Desktop/Auto Insurance/z.jpg'
+result = input("Upload image")
+#result = 'https://upload.wikimedia.org/wikipedia/commons/4/46/Car_Accident.jpg'
+if(validators.url(result)==True):
+    name = os.path.basename(result)
+    full_name = cwd + name
+    path = full_name
+#url = 'https://upload.wikimedia.org/wikipedia/commons/4/46/Car_Accident.jpg'
+#urllib.request.urlretrieve(url,full_name)
+#path = '/home/mandeep/Desktop/Auto Insurance/z.jpg'
 
 from keras.preprocessing import image
 image_path = path
