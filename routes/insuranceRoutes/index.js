f550c32f70773f35df67e70b07bd8522a7821b8c
sonu <sonusaikishan@gmail.com>
2018-10-31 10:56:56 +0530

    Some changes

--- routes/insuranceRoutes/index.js
+++ routes/insuranceRoutes/index.js
@@ -54,7 +54,7 @@ router.get("/api/has-insurance", getAuthToken, (req,res) => {
 
 });
 
-router.post("/api/insurance", getAuthToken, (req,res) => {
+/*router.post("/api/insurance", getAuthToken, (req,res) => {
 
     jwt.verify(req.token, SECRET, (err, authData) => {
         
@@ -66,34 +66,11 @@ router.post("/api/insurance", getAuthToken, (req,res) => {
         {
             const id = authData.id;
 
-            pool.query(`
-                SELECT * FROM "Users" WHERE id = $1
-            `,
-            [id],
-            (err,result) => {
-
-                if(err)
-                {
-                    res.status(500).send(err.toString());
-                }
-                else
-                {
-                    if(result.rows.length === 0)
-                    {
-                      res.status(404).send("User not found!");
-                    }
-                    else
-                    {
-                        const hasInsurance = result.rows[0].hasInsurance;
-                        res.status(200).send(hasInsurance);
-                    }
-                }
-
-            })
+            
         }
 
     });
 
-});
+});*/
 
 module.exports = router;
