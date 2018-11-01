6c163e93b19b7fdfd8d3f345d5496aae0642563a
sonu <sonusaikishan@gmail.com>
2018-11-01 10:45:20 +0530

    Some changes(working)

--- helper/DBconfig.js
+++ helper/DBconfig.js
@@ -1,5 +1,5 @@
 const config = {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: 'postgres://wvnisaaerieheb:38f676583a4fda805d30396063678736fb3c86b98da7af14e8da6a3b00133bf3@ec2-54-243-46-32.compute-1.amazonaws.com:5432/d392dghjufe8pf',
     ssl: true
 };
 
