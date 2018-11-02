ec370a74ed62242fe4904b1cee4e6a85ff2ebaaf
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2018-11-02 23:39:58 +0530

    Some changes

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,28 +1,43 @@
 import React, { Component } from "react";
-import axios from "axios";
+import {
+  InputGroup,
+  InputGroupText,
+  InputGroupAddon,
+  Input,
+  Button
+} from "reactstrap";
 
 class Upload extends Component {
   render() {
     return (
-      <div>
+      <div className="form">
         <form action="/api/claim" method="POST" enctype="multipart/form-data">
           <div style={{ display: "none" }}>
-            <input
+            <Input
+              className="input"
               type="text"
               name="token"
               value={`Bearer ${localStorage.getItem("token")}`}
             />
           </div>
+
           <div>
-            <input type="text" name="name" />
+            <br />
+            <h3>Please upload the image of the damaged vehicle</h3>
+            <br />
+            <h5>Name of file</h5>
+            <Input className="input" type="text" name="name" />
+          </div>
+          <br />
+          <div>
+            <h5>Upload file</h5>
+            <Input className="input" type="file" name="image" />
           </div>
           <div>
-            <input type="file" name="image" />
-          </div>
-          <div>
-            <button type="submit" class="btn">
+            <br />
+            <Button color="primary" type="submit" class="btn">
               Submit
-            </button>
+            </Button>
           </div>
         </form>
       </div>
