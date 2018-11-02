2e8642e076e045f8f44e958a0b46bb260af5e887
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2018-11-02 23:14:57 +0530

    Some changes

--- routes/insuranceRoutes/index.js
+++ routes/insuranceRoutes/index.js
@@ -1,90 +1,74 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
 
 const pg = require("pg");
-const DBconfig = require('../../helper/DBconfig');
+const DBconfig = require("../../helper/DBconfig");
 
 const pool = new pg.Pool(DBconfig);
 
 const jwt = require("jsonwebtoken");
-const SECRET = require('../../helper/JWTsecret');
+const SECRET = require("../../helper/JWTsecret");
 
 const getAuthToken = require("../../middleware/authToken");
 
 const router = express.Router();
 
-router.get("/api/has-insurance", getAuthToken, (req,res) => {
+router.get("/api/has-insurance", getAuthToken, (req, res) => {
+  jwt.verify(req.token, SECRET, (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      const id = authData.id;
 
-    jwt.verify(req.token, SECRET, (err, authData) => {
-        
-        if(err)
-        {
-            res.sendStatus(403);
-        }
-        else
-        {
-            const id = authData.id;
-
-            pool.query(`
+      pool.query(
+        `
                 SELECT * FROM "user" WHERE id = $1
             `,
-            [id],
-            (err,result) => {
-
-                if(err)
-                {
-                    res.status(500).send(err.toString());
-                }
-                else
-                {
-                    if(result.rows.length === 0)
-                    {
-                      res.status(404).send("User not found!");
-                    }
-                    else
-                    {
-                        const hasInsurance = result.rows[0].hasInsurance;
-                        res.status(200).send(hasInsurance);
-                    }
-                }
-
-            })
+        [id],
+        (err, result) => {
+          if (err) {
+            res.status(500).send(err.toString());
+          } else {
+            if (result.rows.length === 0) {
+              res.status(404).send("User not found!");
+            } else {
+              const hasInsurance = result.rows[0].hasInsurance;
+              res.status(200).send(hasInsurance);
+            }
+          }
         }
-
-    });
-
+      );
+    }
+  });
 });
 
-router.post("/api/insurance", getAuthToken, (req,res) => {
+router.post("/api/insurance", getAuthToken, (req, res) => {
+  jwt.verify(req.token, SECRET, (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      const id = authData.id;
 
-    jwt.verify(req.token, SECRET, (err, authData) => {
-        
-        if(err)
-        {
-            res.sendStatus(403);
-        }
-        else
-        {
-            const id = authData.id;
+      const {
+        name,
+        dl_number,
+        car_model,
+        car_company,
+        car_number,
+        car_year,
+        car_price
+      } = req.body;
 
-            const {
-                name,
-                dl_number,
-                car_model,
-                car_company,
-                car_number,
-                car_year,
-                car_price
-            } = req.body;
+      const d = new Date();
+      const current_year = Number(d.getFullYear());
+      const current_date =
+        d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
 
-            const d = new Date();
-            const current_year = Number(d.getFullYear());
-            const current_date = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
+      const yearDiff = current_year - Number(car_year);
+      const current_price = car_price - Number(car_price) * 0.2 * yearDiff;
 
-            const yearDiff = current_year - Number(car_year);
-            const current_price = car_price - ((Number(car_price) * 0.2) * yearDiff);
-
-            pool.query(`
+      pool.query(
+        `
             UPDATE "user" SET
             "hasInsurance" = true,
             "name" = $1,
@@ -98,97 +82,91 @@ router.post("/api/insurance", getAuthToken, (req,res) => {
             "date" = $9
             WHERE "id" = $10;
             `,
-            [name,dl_number,car_model,car_company,car_number,car_year,car_price,current_price,current_date,id],
-            (err, result) => {
-                if(err)
-                {
-                    res.status(500).send(err.toString());
-                }
-                else
-                {
-                    res.status(200).send('Insurance registration succesfully');
-                }
-            });
+        [
+          name,
+          dl_number,
+          car_model,
+          car_company,
+          car_number,
+          car_year,
+          car_price,
+          current_price,
+          current_date,
+          id
+        ],
+        (err, result) => {
+          if (err) {
+            res.status(500).send(err.toString());
+          } else {
+            res.status(200).send("Insurance registration succesfully");
+          }
         }
-
-    });
-
+      );
+    }
+  });
 });
 
-router.get("/api/insurance", getAuthToken, (req,res) => {
-    
-        jwt.verify(req.token, SECRET, (err, authData) => {
-            
-            if(err)
-            {
-                res.sendStatus(403);
-            }
-            else
-            {
-                const id = authData.id;
-    
-                pool.query(`
+router.get("/api/insurance", getAuthToken, (req, res) => {
+  jwt.verify(req.token, SECRET, (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      const id = authData.id;
+
+      pool.query(
+        `
                     SELECT * FROM "user" WHERE id = $1
                 `,
-                [id],
-                (err,result) => {
-    
-                    if(err)
-                    {
-                        res.status(500).send(err.toString());
-                    }
-                    else
-                    {
-                        if(result.rows.length === 0)
-                        {
-                          res.status(404).send("User not found!");
-                        }
-                        else
-                        {
-                            const {
-                                name,
-                                dl,
-                                car_model,
-                                car_company,
-                                car_number,
-                                car_year,
-                                car_price,
-                                car_currentPrice,
-                                date
-                            } = result.rows[0];
+        [id],
+        (err, result) => {
+          if (err) {
+            res.status(500).send(err.toString());
+          } else {
+            if (result.rows.length === 0) {
+              res.status(404).send("User not found!");
+            } else {
+              const {
+                name,
+                dl,
+                car_model,
+                car_company,
+                car_number,
+                car_year,
+                car_price,
+                car_currentPrice,
+                date
+              } = result.rows[0];
 
-                            res.status(200).json({
-                                id,
-                                name,
-                                dl,
-                                car_model,
-                                car_company,
-                                car_number,
-                                car_year,
-                                car_price,
-                                car_currentPrice,
-                                date
-                            });
-                        }
-                    }
-    
-                })
+              res.status(200).json({
+                id,
+                name,
+                dl,
+                car_model,
+                car_company,
+                car_number,
+                car_year,
+                car_price,
+                car_currentPrice,
+                date
+              });
             }
-    
-        });
-    
-    });
+          }
+        }
+      );
+    }
+  });
+});
 
 /*router.get('/py',(req,res) => {
-    
+
     let out = '';
 
     const imagePath = path.join( __dirname, "../../public/images/0010.JPEG" );
     const modelPath = path.join( __dirname, "../../ML/model.h5");
-    
+
     var spawn = require("child_process").spawn;
     var process = spawn('python',["./ML/predict.py", imagePath, modelPath]);
-    
+
     process.stdout.on("data", function(data){
       console.log(data.toString());
       out = out + data.toString();
@@ -197,11 +175,11 @@ router.get("/api/insurance", getAuthToken, (req,res) => {
     process.stderr.on('data', function(data){
         console.log("Error: " + data);
       });
-    
+
     process.stdout.on('end', function(){
       res.send(out);
     });
-   
+
 });*/
 
 module.exports = router;
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-class Upload extends Component 
-{
-
-    render() 
-    {
-        return (
-            <div>
-                <form action="/api/claim" 
-                    method="POST" 
-                    enctype="multipart/form-data" 
-                >
-                    <div style={{ display: "none" }} ><input type="text" name="token" value={`Bearer ${localStorage.getItem("token")}`} /></div>
-                    <div><input type="text" name="name" /></div>
-                    <div><input type="file" name="image" /></div>
-                    <div><button type="submit" class="btn">Submit</button></div>
-                </form>
-            </div>
-        );
-    }
+class Upload extends Component {
+  render() {
+    return (
+      <div>
+        <form action="/api/claim" method="POST" enctype="multipart/form-data">
+          <div style={{ display: "none" }}>
+            <input
+              type="text"
+              name="token"
+              value={`Bearer ${localStorage.getItem("token")}`}
+            />
+          </div>
+          <div>
+            <input type="text" name="name" />
+          </div>
+          <div>
+            <input type="file" name="image" />
+          </div>
+          <div>
+            <button type="submit" class="btn">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Upload;
