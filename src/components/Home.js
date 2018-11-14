7c98a3f4f25f44d979060f3c4e6146749931b735
sonu <sonusaikishan@gmail.com>
2018-11-15 00:08:10 +0530

    Made Design Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Card, CardImg, CardText, CardBody, CardLink,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Card, CardText, CardBody,
+  CardTitle, CardSubtitle, Button,
+  Collapse, Navbar, NavbarToggler,
+  NavbarBrand, Nav, NavItem,
+  NavLink } from 'reactstrap';
 import { withRouter } from "react-router-dom";
 import axios from "axios";
 
 import setAuthToken from "../utils/setAuthToken";
+
+import './Home.css';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       hasInsurance: false,
-      claims: null
+      claims: null,
+      isOpen: false
     };
   }
   componentDidMount() {
@@ -69,6 +74,22 @@ class Home extends Component {
     window.location.assign("/");
   };
 
+  renderDamage = (damageIndex) =>
+  {
+    if(damageIndex === 1)
+    {
+      return "Minor";
+    }
+    else if(damageIndex === 2)
+    {
+      return "Medium";
+    }
+    else if(damageIndex === 3)
+    {
+      return "Severe";
+    }
+  }
+
   renderCliams = () =>
   {
     if(this.state.claims)
@@ -77,17 +98,21 @@ class Home extends Component {
         return (
           <Card key={claim.id} >
             <CardBody>
-              <CardTitle>{claim.name}</CardTitle>
-              <CardSubtitle>{claim.damage}</CardSubtitle>
+              <CardTitle style={{textAlign: "center"}} >{claim.name}</CardTitle>
+              <CardSubtitle>Status - {claim.status}</CardSubtitle>
             </CardBody>
             <img 
               width="100%" 
+              style={{
+                maxHeight: "250px",
+                objectFit: "contain"
+              }}
               src={`http://localhost:8000/images/${claim.image_url}`} 
               alt="car"
             />
             <CardBody>
-              <CardText>{claim.refund}</CardText>
-              <CardText>{claim.status}</CardText>
+              <CardText>Predicted Refund - {claim.refund} Rs</CardText>
+              <CardText>Predicted Damage - {this.renderDamage(claim.damage)}</CardText>
             </CardBody>
           </Card>
         );
@@ -99,50 +124,75 @@ class Home extends Component {
     }
   }
 
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
+      <div>
+      <Navbar color="light" light expand="md">
+          <NavbarBrand href="/home">Car Insurance Assistance</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem className="nav-item" >
+                { 
+                  !this.state.hasInsurance 
+                  ?
+                  (
+                    <Button onClick={this.handleBuyClick} color="info">
+                      Buy Insurance
+                    </Button>
+                  )
+                  :
+                  (
+                    <Button color="info" onClick={this.handleViewInsuranceClick}>
+                      View Insurance
+                    </Button>
+                  )
+                }
+              </NavItem>
+              <NavItem>
+              <Button
+                className="logout"
+                color="danger"
+                onClick={this.handleLogoutClick}
+              >
+                Logout
+              </Button>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
       <center>
         <div>
-          <br />
-
-          <h3>Welcome to Car Insurance Assistance</h3>
-          <Button
-            className="logout"
-            color="danger"
-            onClick={this.handleLogoutClick}
-          >
-            Logout
-          </Button>
-          <br />
-          <br />
-          {!this.state.hasInsurance ? (
+          {this.state.hasInsurance && (
             <div>
-              <h4>Buy an Insurance for your vehicle</h4>
               <br />
-              <Button onClick={this.handleBuyClick} color="info">
-                Buy
-              </Button>
-            </div>
-          ) : (
-            <div>
               <div>
-                <Button color="info" onClick={this.handleViewInsuranceClick}>
-                  View Insurance
-                </Button>
-                <br />
-                <br />
-                <h3>Create a Claim</h3>
-                <Button color="primary" onClick={this.handleCreateClick}>
-                  Create
-                </Button>
+                <div className="header-container">
+                  <div>
+                    <h3>Your Claims</h3>
+                  </div>
+                  <div>
+                    <Button color="primary" onClick={this.handleCreateClick}>
+                      Create a New Claim
+                    </Button>
+                  </div>
+                </div>
+                <div>
+                  {this.renderCliams()}
+                </div>
               </div>
-              <br />
-              <h3>Your Claims</h3>
-              {this.renderCliams()}
             </div>
           )}
         </div>
       </center>
+    </div>
     );
   }
 }
